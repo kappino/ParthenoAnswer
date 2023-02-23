e79cbd583db8c24d072859b3b3b5e851671db563
Riccardo <riccardo.aiello001@studenti.uniparthenope.it>
2023-02-23 22:17:35 +0100

    modified:   static/script.js 	modified:   static/styles.css 	modified:   templates/base.html

--- static/script.js
+++ static/script.js
@@ -2,8 +2,8 @@ const toggle =  document.getElementById('toggleDark');
 const body = document.querySelector('body');
 
 toggle.addEventListener('click', function(){
-    this.classList.toggle('bi-moon');
-    if(this.classList.toggle('bi-brightness-high-fill')){
+    this.classList.toggle('bi-toggle-on');
+    if(this.classList.toggle('bi-toggle-off')){
         body.style.background = "rgb(40, 67, 208)";
         body.style.color = 'black';
         body.style.transition = '1s';
