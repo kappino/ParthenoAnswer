8203b2a7ff65f5c9096ba23099df17d2185cf9b9
Riccardo <riccardo.aiello001@studenti.uniparthenope.it>
2023-02-24 04:24:08 +0100

    modified:   static/script.js 	modified:   static/styles.css 	modified:   templates/base.html 	modified:   templates/comments.html 	modified:   templates/create_cat.html 	modified:   templates/index.html 	modified:   templates/posts.html 	modified:   templates/profile.html 	modified:   templates/search.html 	modified:   templates/subject.html

--- static/script.js
+++ static/script.js
@@ -3,12 +3,12 @@ const body = document.querySelector('body');
 
 toggle.addEventListener('click', function(){
     this.classList.toggle('bi-toggle-on');
-    if(this.classList.toggle('bi-toggle-off')){
-        body.style.background = "rgb(40, 67, 208)";
-        body.style.color = 'black';
-        body.style.transition = '1s';
-    }else{
-        body.style.background = '#242526';
-        body.style.transition = '1s';
+    if(this.classList.toggle('bi-toggle-off'))
+    {
+        body.classList.toggle('dark');
+        body.style.transition = '2s';
+    }else {
+        body.classList.toggle('dark');
+        body.style.transition = '2s';
     }
 })
